efe1e41d168086e0158dd42c73a838ba549c7bbb
stat0123 <kalyazidi@ksu.edu.sa>
2026-02-14 16:59:09 +0300

    measurements.R

--- measurements.R
+++ measurements.R
@@ -1,4 +1,11 @@
 data<-c(28, 30, 29, 31, 32, 30,28)
-mean(data)
-median(data)
-names(table(data)[table(data) == max(table(data))])
+mean=mean(data)
+mean
+
+
+median=median(data)
+median
+
+
+mode=names(table(data)[table(data) == max(table(data))])
+mode
